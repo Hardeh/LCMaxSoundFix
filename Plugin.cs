fc2d1749678c8db9309e2c74733da7067a3a105a
Hardy <hardstuph@gmail.com>
2024-02-06 01:55:48 +0300

    Добавьте файлы проекта.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace LCMaxSoundFix
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+
+            var audioSettings = AudioSettings.GetConfiguration();
+            // default - 40
+            audioSettings.numRealVoices = 80;
+            // default - 512
+            audioSettings.numVirtualVoices = 1024;
+            AudioSettings.SetConfiguration(audioSettings);
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded, new audio settings are applied.");
+
+        }
+    }
+}
